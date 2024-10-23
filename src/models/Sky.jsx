ced71bf0179aa557343915686c516fafbf01c22c
kellysong1 <149431845+kk4song@users.noreply.github.com>
2024-10-23 13:25:11 -0400

    sky not working + lighting

---
+++ src/models/Sky.jsx
@@ -0,0 +1,16 @@
+import {useGLTF} from '@react-three/drei'
+
+
+import React from 'react'
+
+import skyScene from '../assets/3d/sky.glb'
+
+
+const Sky = () => {
+  const skyScene =
+  return (
+    <mesh></mesh>
+  )
+}
+
+export default Sky
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
+{/*import Sky from '@react-three/drei'*/}
 
 import Island from '../models/Island'
 
@@ -36,12 +37,12 @@ const Home = () => {
             camera = {{near: 0.1, far: 1000}}
         >
             <Suspense fallback = {<Loader/>}>
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight  position = {[1, 1, 1]} intensity={2}/>
+                <ambientLight intensity = {0.5}/>
+                <hemisphereLight skyColor = "#b1e1ff" groundColor = "#000000" 
+                intensity = {1}/>
 
+                {/* <Sky /> */}
                 <Island
                 position = {islandPosition}
                 scale = {islandScale}
